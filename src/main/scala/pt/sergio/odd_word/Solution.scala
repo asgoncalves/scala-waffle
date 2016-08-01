3dbc8ab5e2ef3f7b19def8556e3b05cd6f6d507f
Sergio Goncalves <asgoncalves@me.com>
2016-08-01 09:32:06 +0200

    improved scala code

--- src/main/scala/pt/sergio/odd_word/Solution.scala
+++ src/main/scala/pt/sergio/odd_word/Solution.scala
@@ -2,19 +2,14 @@ package pt.sergio.odd_word
 
 object Solution {
 
+  // TODO recursive
   def process(sentence: String): String = {
     val a = sentence.split(" ")
-    var r = Vector(): Vector[String]
-    for(i: Int <- a.indices) {
-      i match {
-        case x if i % 2 != 0 => r = r :+ invert(a(i))
-        case z if i % 2 == 0 => r = r :+ a(i)
-      }
-    }
-    r.mkString(" ")
+    a.indices.foldLeft(Vector.empty[String]) {
+      case (v, e) if e % 2 != 0 => v :+ invert(a(e))
+      case (v, e) => v :+ a(e)
+    }.mkString(" ")
   }
 
   def invert(s: String): String = (for (i <- s.length - 1 to 0 by -1) yield s(i)).mkString
-
-  // TODO recursive
 }
