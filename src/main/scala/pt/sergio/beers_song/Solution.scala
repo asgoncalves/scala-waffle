ff337b4cc06e97976fb5bf2ac60c4734312fca60
Sergio Goncalves <asgoncalves@me.com>
2016-08-02 10:24:55 +0200

    refactored 99 beers

--- src/main/scala/pt/sergio/beers_song/Solution.scala
+++ src/main/scala/pt/sergio/beers_song/Solution.scala
@@ -2,13 +2,9 @@ package pt.sergio.beers_song
 
 object Solution {
   def sing() = {
-    var r: Vector[String] = Vector()
-    99 to 0 by -1 foreach { n =>
-      n match {
-        case 0 => r = r :+ "No more bottles of beer on the wall. No more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall."
-        case _ => r = r :+ f"$n%d bottles of beer on the wall. $n%d bottles of beer.\nTake one down, pass it around.\n${n - 1}%d bottles of beer on the wall."
-      }
+    (99 to 0 by -1).foldLeft(Vector.empty[String]) {
+      case (v, e) if e == 0 => v :+ "No more bottles of beer on the wall. No more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall."
+      case (v, e) => v :+ f"$e%d bottles of beer on the wall. $e%d bottles of beer.\nTake one down, pass it around.\n${e - 1}%d bottles of beer on the wall."
     }
-    r
   }
 }
